cfa9feb6369bd8df6295839de00fbff174a82873
Subekshya Dani <subekshya18in@gmail.com>
2025-03-05 22:30:06 +0545

    Create route.js

---
+++ route.js
@@ -0,0 +1,7 @@
+// route.js
+
+// Check if the current URL is '/longed-peace'
+if (window.location.pathname === "/longed-peace") {
+  // Redirect to the actual poem page
+  window.location.href = "/poetry/poems/poem1.html";
+}
