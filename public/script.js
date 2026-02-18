275e6f577b3162ccfe39c705ec3ece56e4f3ce3b
Subekshya Dani <subekshya18in@gmail.com>
2026-02-18 22:15:39 +0545

    change

--- public/script.js
+++ public/script.js
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-/*  slick Caraousel*/
+/*  slick Caraousel
 
 $('.post-wrapper').slick({
   slidesToShow: 3,
@@ -86,7 +86,32 @@ responsive: [
 // COMMENTS LOGIC
 // =======================
 
+*/
+ /* =====================
+     SLICK SLIDER
+  ===================== */
+  if ($(".post-wrapper").length) {
+    $(".post-wrapper").slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      nextArrow: $(".next"),
+      prevArrow: $(".prev"),
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: { slidesToShow: 2 }
+        },
+        {
+          breakpoint: 768,
+          settings: { slidesToShow: 1 }
+        }
+      ]
+    });
+  }
 
+});
 
 
 
