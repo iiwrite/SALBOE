85adca6539e6f69d9a5d317a5cb2ee8f28c749ef
Subekshya Dani <subekshya18in@gmail.com>
2026-02-18 21:42:53 +0545

    phone commit

--- public/script.js
+++ public/script.js
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-/*  slick Caraousel*/
+/*  slick Caraousel
 
 $('.post-wrapper').slick({
   slidesToShow: 3,
@@ -81,6 +81,33 @@ responsive: [
   // settings: "unslick"
   // instead of a settings object
 ]
+  */
+ $('.post-wrapper').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  nextArrow: $('.next'),
+  prevArrow: $('.prev'),
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
 
 // =======================
 // COMMENTS LOGIC
