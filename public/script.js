093c6e0e26af2e9ab5caa66ee2774ea78acd17a0
Subekshya Dani <subekshya18in@gmail.com>
2026-02-18 21:52:10 +0545

    phone

--- public/script.js
+++ public/script.js
@@ -82,37 +82,28 @@ responsive: [
   // instead of a settings object
 ]
   */
- $('.post-wrapper').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  nextArrow: $('.next'),
-  prevArrow: $('.prev'),
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+ if ($(".post-wrapper").length) {
+    $(".post-wrapper").slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      nextArrow: $(".next"),
+      prevArrow: $(".prev"),
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: { slidesToShow: 2 }
+        },
+        {
+          breakpoint: 768,
+          settings: { slidesToShow: 1 }
+        }
+      ]
+    });
+  }
+
 });
-
-// =======================
-// COMMENTS LOGIC
-// =======================
-
 
 
 
